2ea7969bc6e55c5b04a7e60c54443b34ba4fcf27
Henrique C. de Andrade <161765343+henriquecarvalhodeandrade@users.noreply.github.com>
2026-01-16 20:41:24 +0000

    adicionando inscricoes no db

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-import os
 from sqlalchemy import create_engine, text
+import os
 
 # O Replit já injeta os Secrets no environment
 db_url = os.environ.get('DATABASE_URL')
@@ -16,34 +16,33 @@ engine = create_engine(
 )
 
 def carrega_vagas_db():
-    try:
-        with engine.connect() as conn:
-            resultado = conn.execute(text("SELECT * FROM vagas"))
-
-            vagas = []
-            for vaga in resultado.all():
-                vagas.append(vaga._asdict())
-            return vagas
-
-    except Exception as e:
-        return(f"Erro na conexão: {e}")
+  with engine.connect() as conn:
+    resultado = conn.execute(text("SELECT * FROM vagas"))
+    vagas = []
+    for vaga in resultado.all():
+      vagas.append(vaga._asdict())
+    return vagas
 
 def carrega_vaga_db(id):
-    try:
-        with engine.connect() as conn:
-            resultado = conn.execute(text(
-                f"SELECT * FROM vagas WHERE id = :val"
-            ), 
-                {"val": id}
-            )
-            
-            registro = resultado.mappings().all()
-            
-            if len(registro) == 0:
-                return None
-                
-            else:
-                return dict(registro[0])
-            
-    except Exception as e:
-        return(f"Erro na conexão: {e}")
+  with engine.connect() as conn:
+    resultado = conn.execute(text(f"SELECT * FROM vagas WHERE id = :val"),
+                             {"val": id})
+    registro = resultado.mappings().all()
+    if len(registro) == 0:
+      return None
+    else:
+      return dict(registro[0])
+
+def adiciona_inscricao(id_vaga, dados):
+  with engine.connect() as conn:
+    query = text(
+      f"INSERT INTO inscricoes(vaga_id, nome, email, linkedin, experiencia) VALUES(:vaga_id, :nome, :email, :linkedin, :experiencia)"
+    )
+    conn.execute(
+      query, {
+        'vaga_id': id_vaga,
+        'nome': dados['nome'],
+        'email': dados['email'],
+        'linkedin': dados['linkedin'],
+        'experiencia': dados['experiencia']
+      })
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import carrega_vagas_db, carrega_vaga_db
+from flask import Flask, render_template, jsonify, request
+from database import carrega_vagas_db, carrega_vaga_db, adiciona_inscricao
 
 app = Flask(__name__)
 
@@ -15,13 +15,21 @@ def lista_vagas():
   vagas = carrega_vagas_db()
   return jsonify(vagas)
 
+
 @app.route("/vaga/<id>")
 def mostra_vaga(id):
   vaga = carrega_vaga_db(id)
   if not vaga:
-    return "Vaga não encontrada", 404
-  
+    return "Not Found", 404
   return render_template("detalhevaga.html", vaga=vaga)
+
+
+@app.route("/vaga/<id>/inscricao", methods=["GET", "POST"])
+def inscricao_vaga(id):
+  vaga = carrega_vaga_db(id)
+  data = request.form
+  adiciona_inscricao(id, data)
+  return render_template("inscricao_concluida.html", vaga=vaga, inscricao=data)
 
 
 if __name__ == "__main__":
